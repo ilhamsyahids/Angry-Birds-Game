6077919934081a7308fca3efdd5d775d3eb19ffd
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-02-06 23:11:09 +0700

    add Bird script to control bird

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public enum BirdState { Idle, Thrown }
+    public GameObject Parent;
+    public Rigidbody2D RigidBody;
+    public CircleCollider2D Collider;
+
+    private BirdState _state;
+    private float _minVelocity = 0.05f;
+    private bool _flagDestroy = false;
+    void Start()
+    {
+        RigidBody.bodyType = RigidbodyType2D.Kinematic;
+        Collider.enabled = false;
+        _state = BirdState.Idle;
+    }
+
+    private void FixedUpdate() {
+        if (_state == BirdState.Idle
+            && RigidBody.velocity.sqrMagnitude >= _minVelocity) {
+            _state = BirdState.Thrown;
+        }
+        if (_state == BirdState.Thrown
+            && RigidBody.velocity.sqrMagnitude < _minVelocity
+            && !_flagDestroy) {
+            // destroy after 2s
+            _flagDestroy = true;
+            StartCoroutine(DestroyAfter(2));
+        }
+    }
+
+    private IEnumerator DestroyAfter(float second) {
+        yield return new WaitForSeconds(second);
+        Destroy(gameObject);
+    }
+
+    public void MoveTo(Vector2 target, GameObject parent) {
+        gameObject.transform.SetParent(parent.transform);
+        gameObject.transform.position = target;
+    }
+
+    public void Shoot(Vector2 velocity, float dist, float speed) {
+        Collider.enabled = true;
+        RigidBody.bodyType = RigidbodyType2D.Dynamic;
+        RigidBody.velocity = velocity * speed * dist;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
