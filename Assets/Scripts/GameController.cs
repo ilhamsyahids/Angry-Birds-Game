b4c3863f8f0763dc263d3df47372323b18a8b8b9
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-02-06 23:16:41 +0700

    add game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public List<Bird> Birds;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
