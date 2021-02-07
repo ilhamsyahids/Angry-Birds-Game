8650b7e299c71a1c2bb856be21dcb81bc93e1c85
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-02-07 07:26:06 +0700

    add action when enemy collision

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,6 +15,23 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        if (other.gameObject.tag == "Bird") {
+            _isHit = true;
+            Destroy(gameObject);
+        } else if (other.gameObject.tag == "Obstacle") {
+            float damage = other.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+            Health -= damage;
+
+            if (damage <= 0) {
+                _isHit = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
