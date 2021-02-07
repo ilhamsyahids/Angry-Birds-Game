b2a4d9c9df9a0d7b275b85c5522aa43df510d339
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-02-07 07:57:46 +0700

    connect trail to the bird in game controller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public SlingShooter SlingShooter;
+    public TrailController TrailController;
     public List<Bird> Birds;
     public List<Enemy> Enemies;
     private bool _isGameEnded = false;
@@ -12,12 +13,18 @@ public class GameController : MonoBehaviour
         for (int i = 0; i < Birds.Count; i++)
         {
             Birds[i].OnBirdDestroy += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
         }
         for (int i = 0; i < Enemies.Count; i++)
         {
             Enemies[i].OnEnemyDestroy += CheckGameEnd;
         }
         SlingShooter.InitiateBird(Birds[0]);
+    }
+
+    public void AssignTrail(Bird bird) {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
     }
 
     public void ChangeBird() {
