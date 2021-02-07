94505d3245f771d13eef11cfae4774baed67d9c7
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-02-07 07:38:43 +0700

    fix change call event delegate to OnDestroy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,7 +8,7 @@ public class Enemy : MonoBehaviour
     public float Health = 50f;
     public UnityAction<GameObject> OnEnemyDestroy = delegate { };
     private bool _isHit = false;
-    void Start()
+    void OnDestroy()
     {
         if (_isHit) {
             OnEnemyDestroy(gameObject);
@@ -30,11 +30,5 @@ public class Enemy : MonoBehaviour
                 Destroy(gameObject);
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
