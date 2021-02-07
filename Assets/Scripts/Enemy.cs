a029b6a8c0998d6eabeff64dbe3795c7723987d8
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-02-07 07:25:38 +0700

    add enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    public float Health = 50f;
+    public UnityAction<GameObject> OnEnemyDestroy = delegate { };
+    private bool _isHit = false;
+    void Start()
+    {
+        if (_isHit) {
+            OnEnemyDestroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
